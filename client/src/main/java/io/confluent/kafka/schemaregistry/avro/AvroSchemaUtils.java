cf659c370e4cdadb9f0f12fe8b66a0244abb9072
Robert Yokota <rayokota@gmail.com>
2026-04-10 12:06:46 -0700

    Add Protobuf variant

--- client/src/main/java/io/confluent/kafka/schemaregistry/avro/AvroSchemaUtils.java
+++ client/src/main/java/io/confluent/kafka/schemaregistry/avro/AvroSchemaUtils.java
@@ -26,6 +26,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.util.TokenBuffer;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import io.confluent.avro.type.VariantConversion;
 import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaEntity;
 import java.math.BigDecimal;
 import java.math.BigInteger;
